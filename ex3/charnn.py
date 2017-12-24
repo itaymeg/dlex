7a6c71c0cc66c8cb08ea0e157963b537e388376f
Itay Megera <itaymegera@gmail.com>
2017-12-24 16:17:57 +0200

    Updated CharRNN. Art Version

--- ex3/charnn.py
+++ ex3/charnn.py
@@ -8,63 +8,118 @@ import torch.nn as nn
 from torch.autograd import Variable
 from torch.utils.data import TensorDataset, DataLoader
 from tqdm import tqdm
+import matplotlib.pyplot as plt
+from torch.utils.serialization import load_lua
+import torch.nn.functional as F
 
 dtype = torch.LongTensor
 #load data
 data_dir = './data/'
-train = torchfile.load(os.path.join(data_dir, 'train.t7')) #load train data
-vocab = torchfile.load(os.path.join(data_dir, 'vocab.t7')) #load vocabulary mapping char -> index
-vocab_rev = {a:b for b,a in vocab.items()} # index -> char
-train = Variable(torch.from_numpy(train)).type(dtype)
-labels = train[1:]
+# train = torchfile.load(os.path.join(data_dir, 'train.t7')) #load train data
+# vocab = torchfile.load(os.path.join(data_dir, 'vocab.t7')) #load vocabulary mapping char -> index
+# vocab_rev = {a:b for b,a in vocab.items()} # index -> char
+# train = Variable(torch.from_numpy(train)).type(dtype)
+train = load_lua(os.path.join(data_dir, 'train.t7'))
+vocab = load_lua(os.path.join(data_dir, 'vocab.t7'))
+
 
 
 class CharRNN(nn.Module):
-    def __init__(self, use_cuda = False, vocab_size = 35, embed_size = 1, hidden_size = 64, n_layers = 128):
+    def __init__(self, use_cuda = False, vocab_size = 35, hidden_size = 64, n_layers = 1, embed_size = 512):
         super(CharRNN, self).__init__()
         self.embed = nn.Embedding(vocab_size, embed_size)
         self.lstm = nn.LSTM(embed_size, hidden_size, n_layers)
         self.fc = nn.Linear(hidden_size, vocab_size)
-        self.softmax = nn.Softmax()
+        self.softmax = F.softmax
         self.use_cuda = use_cuda
-    def forward(self, x):
-        print(x.size())
-        x = x.view(128,1)
-        #mbsize x 
+        self.n_layers = n_layers
+        self.hidden_size = hidden_size
+    def forward(self, x, hidden):
+        batch_size = x.size(0)
         x = self.embed(x)
-        output, (hidden, cell) = self.lstm(x)
-        print('Output ', output.size(), ' Hidden ', hidden.size())
-        x = self.fc(hidden)
+        x = x.view(1, batch_size, -1)
+        output, hidden = self.lstm(x, hidden)
+        x = output.view(batch_size, -1)
+        x = self.fc(x)
         print('FC ' , x.size())
         x = self.softmax(x)
         print('Softmax ' , x.size())
-        print(x)
-        x= x.view(128,35)
-        values, indices = torch.max(x,dim= 1) # by rows
-        print('Indices ' , indices.size())
-        print(indices)
-        return indices
+        # values, indices = torch.max(x, dim=1)  # max per sample in batch
+        # print('Indices ', indices.size())
+        return x, hidden
 
-net = CharRNN()
+    def init_hidden(self, batch_size):
+        if self.use_cuda:
+            return (Variable(torch.zeros(self.n_layers, batch_size, self.hidden_size)).cuda(),
+                    Variable(torch.zeros(self.n_layers, batch_size, self.hidden_size)).cuda())
+        return (Variable(torch.zeros(self.n_layers, batch_size, self.hidden_size)),
+                    Variable(torch.zeros(self.n_layers, batch_size, self.hidden_size)))
+
+def getData(samples, sentence_len, batch_size):
+    x = torch.LongTensor(batch_size, sentence_len)
+    y = torch.LongTensor(batch_size, sentence_len)
+
+    for i in range(batch_size):
+        randIdx = np.random.randint(0, samples.size(0) - sentence_len -1)
+        sentence = samples[randIdx: randIdx + sentence_len + 1]
+        x[i, :] = sentence[:-1]
+        y[i, :] = sentence[1:]
+    return Variable(x), Variable(y)
+
+hidden_size = 512
+sentence_len = 128
+batch_size = 128
+embed_size = hidden_size
+use_cuda = False
+vocab_size = len(vocab)
+layers = 1
+epochs = 10
+
+net = CharRNN(use_cuda, vocab_size, hidden_size, layers, embed_size)
 criterion = nn.NLLLoss()
 optimizer = torch.optim.SGD(net.parameters(), lr=0.01)
 train_mod = train[:len(train) - len(train)%128]
-for epoch in tqdm(range(10)):
-    for i in range(0,len(train_mod),128):
-        optimizer.zero_grad()
-        batch = train_mod[i:i+128]
-        output = net(batch).type(dtype)
-        label = labels[i:i+128].type(dtype)
-        output = output.unsqueeze(0)
-        label = label.unsqueeze(0)
-        print('Output ', output[:5], ' Label ', label[:5])
-        
-        loss = criterion(output, label)
-        loss.backward()
-        optimizer.step()
+saved_train = []
+total_loss = []
+for epoch in tqdm(range(epochs)):
+    x, y = getData(train, sentence_len, batch_size)
+    hidden = net.init_hidden(batch_size)
+    optimizer.zero_grad()
+    loss = 0
+    for charIdx in range(sentence_len):
+        input, label = x[:, charIdx], y[:, charIdx]
+        output, hidden = net(input, hidden)
+        print('Criterion - Output: ', output.size(), ' Label: ', label.size())
+        loss += criterion(output.view(batch_size, -1), label)
+        # saved_train.append(output, label)
+    total_loss.append(loss)
+    loss.backward()
+    optimizer.step()
 
-# TODO : Fix Bugs of Loss        
-# TODO : Random Crops, Mini- Batches for faster work
+plt.plot(range(epochs), total_loss, marker=(4, 0))
+plt.show()
+
+
+
+
+
+
+
+#     for i in range(0,len(train_mod),128):
+#         optimizer.zero_grad()
+#         batch = train_mod[i:i+128]
+#         output = net(batch).type(dtype)
+#         label = labels[i:i+128].type(dtype)
+#         output = output.unsqueeze(0)
+#         label = label.unsqueeze(0)
+#         print('Output ', output[:5], ' Label ', label[:5])
+#
+#         loss = criterion(output, label)
+#         loss.backward()
+#         optimizer.step()
+#
+# # TODO : Fix Bugs of Loss
+# # TODO : Random Crops, Mini- Batches for faster work
 
 
 
